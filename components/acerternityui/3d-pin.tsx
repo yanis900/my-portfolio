da50283a5f014a5c1508f9466688807e88316f61
yanis900 <yanis991@hotmail.com>
2024-06-09 00:02:17 +0100

    project cards link fix

--- components/acerternityui/3d-pin.tsx
+++ components/acerternityui/3d-pin.tsx
@@ -31,10 +31,7 @@ export const PinContainer = ({
 
   return (
     <div
-      className={cn(
-        "relative group/pin cursor-pointer",
-        containerClassName
-      )}
+      className={cn("relative group/pin cursor-pointer", containerClassName)}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
     >
@@ -73,8 +70,9 @@ export const PinPerspective = ({
         <div className="absolute top-0 inset-x-0  flex justify-center">
           <a
             href={href}
-            target={"_blank"}
-            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10 "
+            target="_blank"
+            className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-0.5 px-4 ring-1 ring-white/10"
+            style={{ pointerEvents: "auto" }}
           >
             <span className="relative z-20 text-white text-xs font-bold inline-block py-0.5">
               {title}
